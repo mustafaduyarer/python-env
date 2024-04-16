775b955d88e0c355c4605c861923935279e2394d
mustafaduyarer <mustafaduyarer@gmail.com>
2024-04-16 11:54:31 +0200

    first commit

---
+++ invoice.py
@@ -0,0 +1,19 @@
+class Garage:
+
+    def open_door(self):
+        return "The door opens"
+
+home = Garage()
+
+
+# class Invoice:
+#
+#     def greeting(self):
+#         return 'Hi there'
+#
+#
+# inv_one = Invoice()
+# print(inv_one.greeting())
+#
+# inv_two = Invoice()
+# print(inv_two.greeting())
