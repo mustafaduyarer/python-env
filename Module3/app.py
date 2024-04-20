d33531b01f360f4dd42feda93241322493233ad5
mustafaduyarer <mustafaduyarer@gmail.com>
2024-04-21 01:34:18 +0200

    app.py created

--- Module3/app.py
+++ Module3/app.py
@@ -1,8 +1,14 @@
 """
 Environment Setup for Hello World Flask Application
+Hello World Flask Uygulamasının Ortam Kurulumu
 
+Sunucu tarafı API'leri oluşturmak için Python programlama dilinden nasıl yararlanabileceğimizi
+öğreneceğimiz Flask'taki API geliştirme hakkındaki bu kursa hoş geldiniz.
 """
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 @app.route('/')
