db27f488147256ac8cb8d9afd9e8a8f5b5494239
mustafaduyarer <mustafaduyarer@gmail.com>
2024-04-21 01:00:31 +0200

    app.py created

---
+++ Module3/app.py
@@ -0,0 +1,13 @@
+"""
+Environment Setup for Hello World Flask Application
+
+"""
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hey Flask"
+
+if __name__ == '__main__':
+    app.run(debug=True)
